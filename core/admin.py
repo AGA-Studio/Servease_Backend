d776b3b3d87e9f5784039ff9552ece7311a47964
Axel <yerlan_2004@outlook.com>
2026-07-15 18:14:31 -0700

    cambios  de admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import (
+    Empresa, Sucursal, Rol, Categoria, Usuario, Servicio,
+    Postulacion, Oferta, Transaccion, Conversacion, Mensaje, Calificacion
+)
 
-# Register your models here.
+admin.site.register(Empresa)
+admin.site.register(Sucursal)
+admin.site.register(Rol)
+admin.site.register(Categoria)
+admin.site.register(Usuario)
+admin.site.register(Servicio)
+admin.site.register(Postulacion)
+admin.site.register(Oferta)
+admin.site.register(Transaccion)
+admin.site.register(Conversacion)
+admin.site.register(Mensaje)
+admin.site.register(Calificacion)
