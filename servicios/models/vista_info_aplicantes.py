6347c253b0dbb02652f058be465beaf703147b79
Axel <yerlan_2004@outlook.com>
2026-07-24 02:10:04 -0700

    End point para mas de 5 publaciones,contador y fecha de postulantes y validaciones para ver propias publicaciones

--- servicios/models/vista_info_aplicantes.py
+++ servicios/models/vista_info_aplicantes.py
@@ -4,6 +4,7 @@ from django.db import models
 class VistaInfoAplicantes(models.Model):
     id_postulacion = models.IntegerField(primary_key=True)
     servicio_id = models.IntegerField()
+    fecha_postulacion = models.DateTimeField()
     estado_solicitud = models.CharField(max_length=50)
     precio_propuesto = models.DecimalField(max_digits=10, decimal_places=2)
     mensaje_proveedor = models.TextField(blank=True, null=True)
