a9981a0d6b8c34be104e21ee0874993a1bc20aa2
Axel <yerlan_2004@outlook.com>
2026-07-29 11:24:41 -0700

    Base para el dashboard

---
+++ dashBoard/models/__init__.py
@@ -0,0 +1,9 @@
+from .vista_dashboard_proveedor import VistaDashboardProveedor
+from .vista_post_details import VistaPostDetails
+from .vista_trabajos_disponibles import VistaTrabajosDisponibles
+
+__all__ = [
+    'VistaDashboardProveedor',
+    'VistaPostDetails',
+    'VistaTrabajosDisponibles',
+]
